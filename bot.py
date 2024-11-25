5cc0d784c2e971d2006b356ad25298c1de6cbd69
cucumber <coma2ss@gmail.com>
2024-11-25 22:54:29 +0300

    update

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ class Clayton:
                         async with session.get(f"{self.base_url}/assets/{js_file}") as response:
                             response.raise_for_status()
                             js_content = await response.text()
-                            match = re.search(r'_ge="([^"]+)"', js_content)
+                            match = re.search(r'Yge="([^"]+)"', js_content)
                             if match:
                                 self.api_base_id = match.group(1)
                                 return
@@ -85,7 +85,7 @@ class Clayton:
                     await asyncio.sleep(delay)
                 else:
                     return None
-    
+
     async def user_authorization(self, query: str, retries=5):
         url = f'{self.base_url}/api/{self.api_base_id}/user/authorization'
         headers = {
@@ -109,14 +109,45 @@ class Clayton:
                     print(
                         f"{Fore.RED + Style.BRIGHT}HTTP ERROR{Style.RESET_ALL}"
                         f"{Fore.YELLOW + Style.BRIGHT} Retrying... {Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT}[{attempt+1}/{retries}]{Style.RESET_ALL}",
+                        f"{Fore.WHITE + Style.BRIGHT}[{attempt + 1}/{retries}]{Style.RESET_ALL}",
                         end="\r",
                         flush=True
                     )
                     await asyncio.sleep(3)
                 else:
                     return None
-                
+
+    async def save_user(self, query: str, retries=5):
+        url = f'{self.base_url}/api/{self.api_base_id}/user/save-user'
+        headers = {
+            **self.headers,
+            'Content-Length': '0',
+            'Init-Data': query,
+            'Content-Type': 'application/json'
+        }
+
+        for attempt in range(retries):
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.post(url, headers=headers, proxy=self.proxy) as response:
+                        response.raise_for_status()
+                        if response.status == 200:
+                            return await response.json()
+                        else:
+                            return None
+            except (aiohttp.ClientError, aiohttp.ContentTypeError) as e:
+                if attempt < retries - 1:
+                    print(
+                        f"{Fore.RED + Style.BRIGHT}HTTP ERROR{Style.RESET_ALL}"
+                        f"{Fore.YELLOW + Style.BRIGHT} Retrying... {Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT}[{attempt + 1}/{retries}]{Style.RESET_ALL}",
+                        end="\r",
+                        flush=True
+                    )
+                    await asyncio.sleep(3)
+                else:
+                    return None
+
     async def daily_claim(self, query: str, retries=5):
         url = f'{self.base_url}/api/{self.api_base_id}/user/daily-claim'
         headers = {
@@ -140,14 +171,14 @@ class Clayton:
                     print(
                         f"{Fore.RED + Style.BRIGHT}HTTP ERROR{Style.RESET_ALL}"
                         f"{Fore.YELLOW + Style.BRIGHT} Retrying... {Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT}[{attempt+1}/{retries}]{Style.RESET_ALL}",
+                        f"{Fore.WHITE + Style.BRIGHT}[{attempt + 1}/{retries}]{Style.RESET_ALL}",
                         end="\r",
                         flush=True
                     )
                     await asyncio.sleep(3)
                 else:
                     return None
-    
+
     async def all_tasks(self, query: str, type: str, retries=5):
         url = f'{self.base_url}/api/{self.api_base_id}/tasks/{type}'
         headers = {
@@ -170,14 +201,14 @@ class Clayton:
                     print(
                         f"{Fore.RED + Style.BRIGHT}HTTP ERROR{Style.RESET_ALL}"
                         f"{Fore.YELLOW + Style.BRIGHT} Retrying... {Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT}[{attempt+1}/{retries}]{Style.RESET_ALL}",
+                        f"{Fore.WHITE + Style.BRIGHT}[{attempt + 1}/{retries}]{Style.RESET_ALL}",
                         end="\r",
                         flush=True
                     )
                     await asyncio.sleep(3)
                 else:
                     return None
-        
+
     async def start_tasks(self, query: str, task_id: int, retries=5):
         url = f'{self.base_url}/api/{self.api_base_id}/tasks/complete'
         data = json.dumps({'task_id': task_id})
@@ -201,14 +232,14 @@ class Clayton:
                     print(
                         f"{Fore.RED + Style.BRIGHT}HTTP ERROR{Style.RESET_ALL}"
                         f"{Fore.YELLOW + Style.BRIGHT} Retrying... {Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT}[{attempt+1}/{retries}]{Style.RESET_ALL}",
+                        f"{Fore.WHITE + Style.BRIGHT}[{attempt + 1}/{retries}]{Style.RESET_ALL}",
                         end="\r",
                         flush=True
                     )
                     await asyncio.sleep(3)
                 else:
                     return None
-        
+
     async def claim_tasks(self, query: str, task_id: int, retries=5):
         url = f'{self.base_url}/api/{self.api_base_id}/tasks/claim'
         data = json.dumps({'task_id': task_id})
@@ -232,14 +263,14 @@ class Clayton:
                     print(
                         f"{Fore.RED + Style.BRIGHT}HTTP ERROR{Style.RESET_ALL}"
                         f"{Fore.YELLOW + Style.BRIGHT} Retrying... {Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT}[{attempt+1}/{retries}]{Style.RESET_ALL}",
+                        f"{Fore.WHITE + Style.BRIGHT}[{attempt + 1}/{retries}]{Style.RESET_ALL}",
                         end="\r",
                         flush=True
                     )
                     await asyncio.sleep(3)
                 else:
                     return None
-        
+
     async def check_tasks(self, query: str, task_id: int, retries=5):
         url = f'{self.base_url}/api/{self.api_base_id}/tasks/check'
         data = json.dumps({'task_id': task_id})
@@ -263,14 +294,14 @@ class Clayton:
                     print(
                         f"{Fore.RED + Style.BRIGHT}HTTP ERROR{Style.RESET_ALL}"
                         f"{Fore.YELLOW + Style.BRIGHT} Retrying... {Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT}[{attempt+1}/{retries}]{Style.RESET_ALL}",
+                        f"{Fore.WHITE + Style.BRIGHT}[{attempt + 1}/{retries}]{Style.RESET_ALL}",
                         end="\r",
                         flush=True
                     )
                     await asyncio.sleep(3)
                 else:
                     return None
-        
+
     async def user_achievements(self, query: str, retries=5):
         url = f'{self.base_url}/api/{self.api_base_id}/user/achievements/get'
         headers = {
